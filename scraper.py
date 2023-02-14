21f8a3f502e50b64977387f272e14de1436126f9
Naran Babha <nab115@pitt.edu>
2023-02-14 11:33:04 -0800

    automatically extract item name, desc and price from well formed HTML menu

--- scraper.py
+++ scraper.py
@@ -6,7 +6,8 @@ import bs4
 from selenium.webdriver.common.by import By
 from queue import Queue
 
-from utils import get_chrome_driver
+from utils import get_chrome_driver, TopN, Item
+
 
 def run():
 
@@ -16,12 +17,28 @@ def run():
     html = f.read()
     f.close()
 
-    parse_html_text(html)
-
-def parse_html_text(html):
-
     soup = bs4.BeautifulSoup(html, 'html.parser')
     body = soup.find('body')
+
+    [item_desc, item_name, item_price] = find_menu_keys(body)
+
+    [tag, class_name] = item_name.split('|')
+    names = body.find_all(tag, {'class': class_name})
+    for name in names[:5]:
+        print(name.getText() + '\n')
+    
+    [tag, class_name] = item_desc.split('|')
+    names = body.find_all(tag, {'class': class_name})
+    for name in names[:5]:
+        print(name.getText() + '\n')
+    
+    [tag, class_name] = item_price.split('|')
+    names = body.find_all(tag, {'class': class_name})
+    for name in names[:5]:
+        print(name.getText() + '\n')
+
+
+def find_menu_keys(body):
 
     # Traverse through HTML body using BFS or DFS
     q = Queue()
@@ -30,16 +47,16 @@ def parse_html_text(html):
 
     while not q.empty():
         element = q.get()
-        
-        try:
-            children = element.contents
-            text = element.find_all(string=True, recursive=False)
-        except:
-            print(element.contents)
-            print(element.parent.name)
-            print(element.parent.parent.name)
-            break
+
+        if type(element) in [
+            bs4.element.NavigableString
+            , bs4.element.Comment
+            , bs4.element.Stylesheet
+            , bs4.element.Script
+        ]:
             continue
+
+        text = element.find_all(string=True, recursive=False)
 
         if len(text) == 1:
             try: 
@@ -47,19 +64,29 @@ def parse_html_text(html):
             except:
                 class_name = ''
 
-            key = element.name + ' ' + class_name
+            key = element.name + '|' + class_name
             if key in hashmap:
                 hashmap[key][0] = hashmap[key][0] + 1
                 hashmap[key][1] = hashmap[key][1] + len(text[0])
             else:
                 hashmap[key] = [1, len(text[0])]
         
-        for child in children:
+        for child in element.contents:
             q.put(child)
     
+    top3 = TopN(3)
     for k in hashmap:
         hashmap[k][1] = hashmap[k][1] / hashmap[k][0]
-    print(hashmap)
+        top3.push(Item(k, hashmap[k][0]))
+
+    tags = []
+    for item in top3.getTopN():
+        tags.append(Item(item.id, hashmap[item.id][1]))
+    
+    tags.sort(reverse=True)
+
+    return [tag.id for tag in tags]
+
 
 def retrieve_menu():
     driver = get_chrome_driver()
@@ -67,7 +94,6 @@ def retrieve_menu():
     elements = driver.find_elements(By.TAG_NAME, 'html')
     f = open('menu.txt', 'w')
     f.write(elements[0].get_attribute("innerHTML"))
-
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,7 +1,47 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from functools import total_ordering
 
 def get_chrome_driver():
 
     return webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+@total_ordering
+class Item:
+    def __init__(self, id, value):
+        self.id = id
+        self.value = value
+    
+    def __lt__(self, other):
+        return self.value < other.value
+    
+    def __eq__(self, other):
+        return self.value == other.value
+    
+    def __repr__(self):
+        return f'Item(\'{self.id}\', {self.value})'
+
+
+    
+#TODO : rewrite this with heapq
+
+class TopN:
+    def __init__(self, max_size):
+        self.max_size = max_size
+        self.elements = [None] * max_size
+    
+    def push(self, obj):
+
+        insert = obj
+        for i in range(self.max_size):
+            if(self.elements[i] is None):
+                self.elements[i] = insert
+                break
+            elif(self.elements[i] < insert):
+                temp = self.elements[i]
+                self.elements[i] = insert
+                insert = temp
+    
+    def getTopN(self):
+        return self.elements
